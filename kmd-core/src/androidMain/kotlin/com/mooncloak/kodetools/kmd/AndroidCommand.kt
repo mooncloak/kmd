230d1db0ebec1d8e6461d2f834f71e2fa219d825
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-01-28 14:35:02 -0500

    Implemented Android Command.execute function

--- kmd-core/src/androidMain/kotlin/com/mooncloak/kodetools/kmd/AndroidCommand.kt
+++ kmd-core/src/androidMain/kotlin/com/mooncloak/kodetools/kmd/AndroidCommand.kt
@@ -1,3 +1,85 @@
 package com.mooncloak.kodetools.kmd
 
-internal actual suspend fun Command.execute(): CommandResult = TODO()
+import android.os.Build
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+
+@OptIn(ExperimentalKmdApi::class)
+internal actual suspend fun Command.execute(): CommandResult =
+    withContext(Dispatchers.IO) {
+        val commands = (command.commandToValues() + arguments.flatMap { argument ->
+            argument.commandToValues()
+        }).toTypedArray()
+
+        var currentOutput = ProcessOutput()
+        var currentError = ProcessOutput()
+
+        var builder = ProcessBuilder(*commands)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder = builder.redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.PIPE)
+        }
+
+        val process = builder.start()
+
+        launch {
+            process.inputStream.subscribeOutput(
+                output = { currentOutput },
+                onOutputChanged = { updated ->
+                    currentOutput = updated
+
+                    standardOutHandlers.forEach { handler ->
+                        handler.handle(ProcessOutputScope, updated)
+                    }
+                }
+            )
+        }
+
+        launch {
+            process.errorStream.subscribeOutput(
+                output = { currentError },
+                onOutputChanged = { updated ->
+                    currentError = updated
+
+                    standardErrorHandlers.forEach { handler ->
+                        handler.handle(ProcessOutputScope, updated)
+                    }
+                }
+            )
+        }
+
+        val exitCode = ExitCode(value = process.waitFor())
+
+        return@withContext CommandResult(
+            command = command,
+            arguments = arguments,
+            exitCode = exitCode
+        )
+    }
+
+private suspend fun InputStream.subscribeOutput(
+    output: () -> ProcessOutput,
+    onOutputChanged: suspend (ProcessOutput) -> Unit
+) {
+    withContext(Dispatchers.IO) {
+        this@subscribeOutput.bufferedReader()
+            .useLines { lineSequence ->
+                val iterator = lineSequence.iterator()
+
+                while (this.isActive && iterator.hasNext()) {
+                    val line = iterator.next()
+
+                    onOutputChanged(
+                        ProcessOutput(
+                            totalLines = output().totalLines + line,
+                            diffLines = listOf(line)
+                        )
+                    )
+                }
+            }
+    }
+}
